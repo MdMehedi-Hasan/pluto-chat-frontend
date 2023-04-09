8e08faf2258e90a0fce9e8135524e22b68418145
MdMehedi-Hasan <mh19115@gmail.com>
2023-04-09 11:47:18 +0600

    initial connection established

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import io from 'socket.io-client'
 const socket = io.connect("http://localhost:5000");
+console.log(socket);
 
 function App() {
   return (
     <div>
-
+asdfgdgh
     </div>
   );
 }
